14d35298321c6bc39e89becf113186b9b2790f27
蒼時弦也 <elct9620@frost.tw>
2022-05-30 15:56:22 +0800

    Scan html directly to pruner css

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,14 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [ './hugo_stats.json' ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
-});
+const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
      plugins: [
-         ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
+         ...(
+           process.env.HUGO_ENVIRONMENT === 'production' ?
+           [
+             purgecss({
+               content: ['./**/*.html']
+             })
+           ] : []
+         )
      ]
  };
